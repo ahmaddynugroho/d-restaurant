fd395e4d3552df0cc6e45ff69199a2c1052c1132
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-08 02:15:39 +0700

    feat: responsive nav

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,19 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
 
-console.log('Hello Coders! :)');
+/** @param {string} query */
+const q = (query) => document.querySelector(query);
+/** @param {string} query */
+const qa = (query) => document.querySelectorAll(query);
+
+/** @type {HTMLButtonElement} */
+const showNavMenuButton = q("#show-menu");
+/** @type {NodeListOf<HTMLAnchorElement>} */
+const navLinks = qa("nav a");
+showNavMenuButton.addEventListener("click", (e) => {
+  navLinks.forEach((e) => {
+    const currentDisplay = e.style.display;
+    e.style.display =
+      currentDisplay === "inline-block" ? "none" : "inline-block";
+  });
+});
