1459e0f010ddc50eba83d3eee1e7ffbc58f3736d
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-15 00:45:56 +0700

    feat: fetch api

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,3 +1,4 @@
 export const BASE_API = 'https://restaurant-api.dicoding.dev'
 export const GET_RESTAURANT_API = `${BASE_API}/list`
 export const GET_RESTAURANT_IMAGE_API = `${BASE_API}/images`
+export const GET_RESTAURANT_DETAIL_API = `${BASE_API}/detail`
--- src/scripts/pages/detail.js
+++ src/scripts/pages/detail.js
@@ -1,9 +1,19 @@
 import { hideLoadingElement, showLoadingElement } from '../components/loading'
+import { GET_RESTAURANT_DETAIL_API } from '../config'
 import { q } from '../utils/query-selector'
 
-export function renderDetailPage (id) {
+export async function renderDetailPage (id) {
   showLoadingElement()
   const appContainer = q('#app')
+  const restaurantDetail = await fetchDetail(id)
   appContainer.innerHTML = `<h1>ini detail ${id}</h1>`
+  console.debug('done', restaurantDetail)
   hideLoadingElement()
+}
+
+async function fetchDetail (id) {
+  const { restaurant } = await fetch(GET_RESTAURANT_DETAIL_API + '/' + id).then(
+    res => res.json()
+  )
+  return restaurant
 }
