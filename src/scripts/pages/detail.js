60f2334a8aedaca745ec7291f24678a2e5f8ba4b
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-15 01:02:30 +0700

    feat: restaurant detail page

--- src/scripts/pages/detail.js
+++ src/scripts/pages/detail.js
@@ -1,13 +1,40 @@
 import { hideLoadingElement, showLoadingElement } from '../components/loading'
-import { GET_RESTAURANT_DETAIL_API } from '../config'
+import { GET_RESTAURANT_DETAIL_API, GET_RESTAURANT_IMAGE_API } from '../config'
 import { q } from '../utils/query-selector'
+
+const template = restaurant => /* html */ `
+  <h1>${restaurant.name} (${restaurant.rating}⭐)</h1>
+  <img src="${GET_RESTAURANT_IMAGE_API}/large/${restaurant.pictureId}" alt="${
+  restaurant.name
+} image" />
+  <p>City: ${restaurant.city}</p>
+  <p>Address: ${restaurant.address}</p>
+  <p>Description: ${restaurant.description}</p>
+  <p>Categories: ${restaurant.categories.map(v => v.name).join(', ')}</p>
+  <h1>Menu Minuman</h1>
+  <ul>
+    ${restaurant.menus.drinks.map(v => '<li>' + v.name + '</li>').join('')}
+  </ul>
+  <h1>Menu Makanan</h1>
+  <ul>
+    ${restaurant.menus.foods.map(v => '<li>' + v.name + '</li>').join('')}
+  </ul>
+  <h1>Review</h1>
+  <ul>
+    ${restaurant.customerReviews
+      .map(v => `<li>${v.name}: ${v.review} (${v.date})</li>`)
+      .join('')}
+  </ul>
+`
 
 export async function renderDetailPage (id) {
   showLoadingElement()
+
   const appContainer = q('#app')
   const restaurantDetail = await fetchDetail(id)
-  appContainer.innerHTML = `<h1>ini detail ${id}</h1>`
-  console.debug('done', restaurantDetail)
+
+  appContainer.innerHTML = template(restaurantDetail)
+
   hideLoadingElement()
 }
 
