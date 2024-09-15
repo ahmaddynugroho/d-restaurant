1223ca66b870d3dc2bee0ad7f1fa78164d775f83
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-15 21:33:49 +0700

    feat: add or remove fav

--- src/scripts/utils/indexeddb.js
+++ src/scripts/utils/indexeddb.js
@@ -1,7 +1,11 @@
 import { openDB } from 'idb'
+import { q } from './query-selector'
 
 const dbName = 'RestaurantDB'
 const storeName = 'RestaurantStore'
+
+const db = await useDB(dbName, storeName)
+console.debug('db: opened', dbName, storeName)
 
 async function useDB (dbName, storeName) {
   return await openDB(dbName, 1, {
@@ -19,11 +23,34 @@ async function addData (db, data) {
   return await Promise.all([tx.store.add(data), tx.done])
 }
 
-console.debug('db: usedb')
-const db = await useDB(dbName, storeName)
-console.debug('db: adding')
-await addData(db, { id: Date.now().toString(), name: Date.now().toString() })
-console.debug('db: added')
-console.debug('db: adding')
-await addData(db, { id: Date.now().toString(), name: Date.now().toString() })
-console.debug('db: added')
+async function deleteData (db, dataId) {
+  const tx = await db.transaction(storeName, 'readwrite')
+  return await Promise.all([tx.store.delete(dataId), tx.done])
+}
+
+export async function initFavoriteButton (buttonId, restaurant) {
+  let isExist = await db.get(storeName, restaurant.id)
+  const favButton = q(buttonId)
+
+  renderFavoriteButtonText(restaurant.id, favButton)
+  favButton.addEventListener('click', async e => {
+    if (isExist) {
+      await deleteData(db, restaurant.id)
+      console.debug('db: deleted', restaurant.id, restaurant.name)
+    } else {
+      await addData(db, restaurant)
+      console.debug('db: added', restaurant.id, restaurant.name)
+    }
+    isExist = !isExist
+    renderFavoriteButtonText(restaurant.id, favButton)
+  })
+}
+
+async function renderFavoriteButtonText (restaurantId, favButton) {
+  const isExist = await db.get(storeName, restaurantId)
+  if (isExist) {
+    favButton.innerHTML = 'Remove from favorite'
+  } else {
+    favButton.innerHTML = 'Add to favorite'
+  }
+}
