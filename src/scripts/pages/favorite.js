af8d01a4b30b522b557d8d82ac3ca24017a0ee99
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-17 17:47:54 +0700

    fav rest page

--- src/scripts/pages/favorite.js
+++ src/scripts/pages/favorite.js
@@ -1,4 +1,5 @@
-import { showLoadingElement } from "../components/loading";
+import { hideLoadingElement, showLoadingElement } from "../components/loading";
+import { GET_RESTAURANT_IMAGE_API } from "../config";
 import { getAllRestaurant } from "../utils/indexeddb";
 import { q } from "../utils/query-selector";
 
@@ -6,6 +7,27 @@ export async function renderFavoritePage() {
   showLoadingElement()
   
   const appContainer = q('#app')
-  console.log(await getAllRestaurant());
-  
+  appContainer.innerHTML = /* html */ `
+    <h1>Favorite Restaurant</h1>
+    <div id="restaurant-list"></div>
+`
+  const restaurantContainer = q('#restaurant-list')
+  const restaurants = await getAllRestaurant()
+  renderRestaurantList(restaurantContainer, restaurants)
+
+  hideLoadingElement()
+}
+
+function renderRestaurantList (appContainer, restaurants) {
+  restaurants.forEach(r => {
+    appContainer.innerHTML += /* html */ `
+    <a href="#/detail/${r.id}">
+      <div id="restaurant">
+        <p>${r.name}</p>
+        <p>${r.rating}⭐</p>
+        <img src="${GET_RESTAURANT_IMAGE_API}/small/${r.pictureId}" alt="${r.name} image">
+      </div>
+    </a>
+    `
+  })
 }
