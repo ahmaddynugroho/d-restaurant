ddef986602cb20fb5553e6239d960f70196afeeb
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-20 14:57:03 +0700

    feat: add/remove fav

--- src/scripts/utils/indexeddb.js
+++ src/scripts/utils/indexeddb.js
@@ -59,3 +59,60 @@ export async function getAllRestaurant () {
   const tx = await db.transaction(storeName, 'readwrite')
   return (await Promise.all([tx.store.getAll(), tx.done]))[0]
 }
+
+export class FavoriteDB {
+  constructor (dbName, storeName) {
+    this._dbName = dbName
+    this._storeName = storeName
+  }
+
+  async initDB () {
+    this._db = await openDB(this._dbName, 1, {
+      upgrade: db => {
+        if (!db.objectStoreNames.contains(this._storeName)) {
+          const store = db.createObjectStore(this._storeName, { keyPath: 'id' })
+          store.createIndex('id', 'id')
+        }
+      }
+    })
+  }
+
+  _writeTx () {
+    return this._db.transaction(this._storeName, 'readwrite')
+  }
+
+  async add (data) {
+    const tx = this._writeTx()
+    const res = await Promise.all([tx.store.add(data), tx.done])
+    return res[0]
+  }
+  
+  async delete (id) {
+    const tx = this._writeTx()
+    const res = await Promise.all([tx.store.delete(id), tx.done])
+    return res[0]
+  }
+  
+  async get(id) {
+    const tx = this._writeTx()
+    const res = await Promise.all([tx.store.get(id), tx.done])
+    return res[0]
+  }
+}
+
+// TODO: delete this
+async function testFavDB () {
+  console.log('db: start')
+  const db = new FavoriteDB('fav-db', 'fav-store')
+  await db.initDB()
+  // const now = Date.now()
+  const now = 'fake-id-temporary'
+  const id = await db.add({ id: now, name: 'test' })
+  console.log('db: added', id)
+  const resGet = await db.get(now + 'x')
+  console.log('db: get', resGet)
+  const idDel = await db.delete(now)
+  console.log('db: deleted', idDel)
+}
+
+// testFavDB()
